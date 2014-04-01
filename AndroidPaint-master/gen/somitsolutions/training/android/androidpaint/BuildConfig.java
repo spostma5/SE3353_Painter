a71e4fd155c9349293679e1fbd2aa6cc4e089ea6
spostma5 <samspostma@gmail.com>
2014-03-31 22:39:31 -0400

    Initial Commit, Painter program

---
+++ AndroidPaint-master/gen/somitsolutions/training/android/androidpaint/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package somitsolutions.training.android.androidpaint;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
